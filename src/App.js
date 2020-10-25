5aaa454b772fdea808173bdb3a84a0be4606661f
danny237 <dannymagar37@gmail.com>
2020-10-25 23:31:27 +0545

    initial configuration

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Welcome To TODO app !
+    </div>
+  );
+}
+
+export default App;
